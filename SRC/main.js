b09794aaa2848cfac01f4c228d4fa2aaf4729fcc
DominikFrackowiak <86988772+DominikFrackowiak@users.noreply.github.com>
2021-11-09 14:22:44 +0100

    Setup connection with DOM elements and add event listeners

---
+++ SRC/main.js
@@ -0,0 +1,47 @@
+//OBJECT THAT RECOLLECT ALL ELEMENTS
+const viewElems = {};
+
+const getDOMElem = id => document.getElementById(id);
+
+//FUNCTION THAT RECOLLECTS ELEMENTS AND SETS THEM AS KEY/VALUE PAIRS OF "VIEWELEMS" OBJ.
+const connectHTMLElems = () => {
+ viewElems.mainContainer = getDOMElem('');
+ viewElems.weatherSearchView = getDOMElem('wheatherSearchView');
+ viewElems.weatherForecasthView = getDOMElem('wheatherForecastView');
+
+ viewElems.searchInput = getDOMElem('searchInput');
+ viewElems.searchButton = getDOMElem('searchButton');
+
+ viewElems.weatherCityContainer = getDOMElem('weatherCityContainer');
+ viewElems.weatherCity = getDOMElem('weatherCity');
+ viewElems.weatherIcon = getDOMElem('weatherIcon');
+
+ viewElems.weatherCurrentTemp = getDOMElem('weatherCurrentTemp');
+ viewElems.weatherMaxTemp = getDOMElem('weatherMaxTemp');
+ viewElems.weatherMinTemp = getDOMElem('weatherMinTemp');
+
+ viewElems.returnToSearchBtn = getDOMElem('returnToSearchBtn');
+}
+
+//FUNCTION THAT RECOLLECTS LISTENERS
+const setupListeners = () => {
+ viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
+ viewElems.searchButton.addEventListener('click', onClickSubmit);
+}
+
+
+
+//MAIN FUNCTION THAT IS TRIGGERED BY THE END OF THE LOADING OF THE DOCUMENT
+const initializeApp = () => {
+ connectHTMLElems();
+ setupListeners();
+}
+
+const onEnterSubmit = () => {
+ console.log("enter!")
+};
+const onClickSubmit = () => {
+ console.log("Click!")
+};
+
+document.addEventListener("DOMContentLoaded", initializeApp);
